c5885f1cf5378c8be302f769265cb7e10bc114b8
craynafinal <craynafinal@gmail.com>
2019-01-27 13:54:35 -0800

    ocr success

---
+++ DesktopApp/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Windows.Globalization;
+using Windows.Graphics.Imaging;
+using Windows.Media.Ocr;
+
+namespace DesktopApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var language = new Language("ko");
+            if (!OcrEngine.IsLanguageSupported(language))
+            {
+                throw new Exception($"{ language.LanguageTag } is not supported in this system.");
+            }
+            var stream = File.OpenRead("D:\\Data\\Downloads\\Temp\\temp\\ddd.jpg");
+            var decoder = await BitmapDecoder.CreateAsync(stream.AsRandomAccessStream());
+            var bitmap = await decoder.GetSoftwareBitmapAsync();
+            var engine = OcrEngine.TryCreateFromLanguage(language);
+            var ocrResult = await engine.RecognizeAsync(bitmap).AsTask();
+            Console.WriteLine(ocrResult.Text);
+        }
+    }
+}
