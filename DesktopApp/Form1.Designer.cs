8eb4446b680f493917a6118affec02796804c095
craynafinal <craynafinal@gmail.com>
2019-02-06 19:36:17 -0800

    copied some mort scripts, need to refactor

--- DesktopApp/Form1.Designer.cs
+++ DesktopApp/Form1.Designer.cs
@@ -30,13 +30,17 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(444, 152);
+            this.button1.Location = new System.Drawing.Point(445, 152);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(75, 22);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -52,13 +56,49 @@
             this.label1.Text = "하하";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(327, 272);
+            this.button2.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(193, 58);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(67, 175);
+            this.button3.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(193, 58);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(601, 61);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(18F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -70,6 +110,9 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- DesktopApp/Form1.cs
+++ DesktopApp/Form1.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿
+using AForge;
+using System;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using Windows.Globalization;
 using Windows.Graphics.Imaging;
@@ -25,7 +28,7 @@ namespace DesktopApp
 
         private async void test_bg_read()
         {
-            var language = new Language("ko");
+            var language = new Language("ja");
             if (!OcrEngine.IsLanguageSupported(language))
             {
                 throw new Exception($"{ language.LanguageTag } is not supported in this system.");
@@ -34,7 +37,20 @@ namespace DesktopApp
             using (MemoryStream m = new MemoryStream())
             {
                 var bitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
-                Graphics.FromImage(bitmap).CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size, CopyPixelOperation.SourceCopy);
+                //var bitmap = new Bitmap(2, 2, System.Drawing.Imaging.PixelFormat.Format16bppGrayScale);
+                Graphics g = Graphics.FromImage(bitmap);
+                    
+                g.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size, CopyPixelOperation.SourceCopy);
+
+                //bitmap = Image.MakeGrayscale3(bitmap);
+                //new AForge.Imaging.Filters.LevelsLinear().Apply()
+                AForge.Imaging.Filters.HSLLinear grayscaleFilter = new AForge.Imaging.Filters.HSLLinear();
+                grayscaleFilter.OutSaturation = new Range(0, 0);
+                grayscaleFilter.ApplyInPlace(bitmap);
+
+                AForge.Imaging.Filters.ContrastCorrection contrastFilter = new AForge.Imaging.Filters.ContrastCorrection(255);
+                contrastFilter.ApplyInPlace(bitmap);
+
                 bitmap.Save("D:\\Data\\Downloads\\Temp\\temp\\ddd2.jpg");
                 bitmap.Save(m, ImageFormat.Bmp);
                 
@@ -49,10 +65,12 @@ namespace DesktopApp
                 //TODO: url breaks because of special characters
 
                 var watch = System.Diagnostics.Stopwatch.StartNew();
-                string result = papagoTest.Translate(ocrResult.Text, BackgroundApp.LanguageCode.KOREAN, BackgroundApp.LanguageCode.ENGLISH);
+                string result = papagoTest.Translate(ocrResult.Text, BackgroundApp.LanguageCode.JAPANESE, BackgroundApp.LanguageCode.KOREAN);
                 watch.Stop();
                 var elapsedMs = watch.ElapsedMilliseconds;
                 Console.WriteLine(result + " " + elapsedMs);
+
+                g.Flush();
             }
         }
 
@@ -63,8 +81,10 @@ namespace DesktopApp
             {
                 throw new Exception($"{ language.LanguageTag } is not supported in this system.");
             }
-            var stream = File.OpenRead("D:\\Data\\Downloads\\Temp\\temp\\ddd.jpg");
+            var stream = File.OpenRead("D:\\Data\\Downloads\\Temp\\temp\\photoshoped.jpg");
+
             var decoder = await BitmapDecoder.CreateAsync(stream.AsRandomAccessStream());
+
             var bitmap = await decoder.GetSoftwareBitmapAsync();
             var engine = OcrEngine.TryCreateFromLanguage(language);
             var ocrResult = await engine.RecognizeAsync(bitmap).AsTask();
@@ -73,6 +93,139 @@ namespace DesktopApp
 
         private void label1_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            test_file_read();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmap = null;
+            using (var image = new Bitmap("D:\\Data\\Downloads\\Temp\\temp\\zzz.jpg"))
+            {
+                bitmap = new Bitmap(image);
+                
+                //bitmap = Image.MakeGrayscale3(bitmap);
+                //new AForge.Imaging.Filters.LevelsLinear().Apply()
+                AForge.Imaging.Filters.HSLLinear grayscaleFilter = new AForge.Imaging.Filters.HSLLinear();
+                grayscaleFilter.OutSaturation = new Range(0, 0);
+                grayscaleFilter.ApplyInPlace(bitmap);
+                
+                //-50
+                AForge.Imaging.Filters.BrightnessCorrection brightnessFilter
+                    = new AForge.Imaging.Filters.BrightnessCorrection(-70);
+                brightnessFilter.ApplyInPlace(bitmap);
+
+                //50
+                AForge.Imaging.Filters.ContrastCorrection contrastFilter
+                    = new AForge.Imaging.Filters.ContrastCorrection(70);
+                contrastFilter.ApplyInPlace(bitmap);
+
+                bitmap.Save("D:\\Data\\Downloads\\Temp\\temp\\ddd2.jpg");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // completely broken at this moment!
+
+            //instance = this;
+            InitializeComponent();
+
+            this.MouseDown += new MouseEventHandler(mouse_Click);
+            this.MouseMove += new MouseEventHandler(mouse_Move);
+
+            this.Location = SystemInformation.VirtualScreen.Location;
+            this.Size = SystemInformation.VirtualScreen.Size;
+            g = this.CreateGraphics();
+        }
+
+
+        public bool LeftButtonDown = false;
+        public int RectangleHeight = new int();
+        public int RectangleWidth = new int();
+        public bool RectangleDrawn = false;
+
+        public System.Drawing.Point ClickPoint = new System.Drawing.Point();
+        public System.Drawing.Point CurrentTopLeft = new System.Drawing.Point();
+        public System.Drawing.Point CurrentBottomRight = new System.Drawing.Point();
+        public System.Drawing.Point DragClickRelative = new System.Drawing.Point();
+
+        Pen EraserPen = new Pen(Color.FromArgb(255, 255, 192), 1);
+        Pen MyPen = new Pen(Color.Black, 1);
+
+        Graphics g;
+
+        private void mouse_Click(object sender, MouseEventArgs e)
+        {
+
+            if (e.Button == MouseButtons.Left)
+            {
+                LeftButtonDown = true;
+                ClickPoint = new System.Drawing.Point(System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y);
+
+                if (RectangleDrawn)
+                {
+
+                    RectangleHeight = CurrentBottomRight.Y - CurrentTopLeft.Y;
+                    RectangleWidth = CurrentBottomRight.X - CurrentTopLeft.X;
+                    DragClickRelative.X = Cursor.Position.X - CurrentTopLeft.X;
+                    DragClickRelative.Y = Cursor.Position.Y - CurrentTopLeft.Y;
+
+                }
+            }
+        }
+        
+        private void mouse_Move(object sender, MouseEventArgs e)
+        {
+            if (LeftButtonDown && !RectangleDrawn)
+            {
+                DrawSelection();
+            }
+        }
+
+        private void DrawSelection()
+        {
+            this.Cursor = Cursors.Arrow;
+
+            //Erase the previous rectangle
+            g.DrawRectangle(EraserPen, CurrentTopLeft.X - this.Location.X, CurrentTopLeft.Y - this.Location.Y, CurrentBottomRight.X - CurrentTopLeft.X, CurrentBottomRight.Y - CurrentTopLeft.Y);
+
+            //Calculate X Coordinates
+            if (Cursor.Position.X < ClickPoint.X)
+            {
+
+                CurrentTopLeft.X = Cursor.Position.X;
+                CurrentBottomRight.X = ClickPoint.X;
+
+            }
+            else
+            {
+
+                CurrentTopLeft.X = ClickPoint.X;
+                CurrentBottomRight.X = Cursor.Position.X;
+
+            }
+
+            //Calculate Y Coordinates
+            if (Cursor.Position.Y < ClickPoint.Y)
+            {
+
+                CurrentTopLeft.Y = Cursor.Position.Y;
+                CurrentBottomRight.Y = ClickPoint.Y;
+
+            }
+            else
+            {
+                CurrentTopLeft.Y = ClickPoint.Y;
+                CurrentBottomRight.Y = Cursor.Position.Y;
+            }
+
+            //Draw a new rectangle
+            g.DrawRectangle(MyPen, CurrentTopLeft.X - this.Location.X, CurrentTopLeft.Y - this.Location.Y, CurrentBottomRight.X - CurrentTopLeft.X, CurrentBottomRight.Y - CurrentTopLeft.Y);
 
         }
     }
