f64f9b7e34f1b954272efa2412d636d4e5f3ac2c
craynafinal <craynafinal@gmail.com>
2019-02-10 19:53:50 -0800

    adding drawing file

---
+++ DesktopApp/Drawing.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Drawing;
+
+namespace DesktopApp
+{
+    class Drawing
+    {
+        [DllImport("User32.dll")]
+        public static extern IntPtr GetDC(IntPtr hwnd);
+        [DllImport("User32.dll")]
+        public static extern void ReleaseDC(IntPtr hwnd, IntPtr dc);
+
+        public void DrawGraphic()
+        {
+            IntPtr desktopPtr = GetDC(IntPtr.Zero);
+            Graphics g = Graphics.FromHdc(desktopPtr);
+
+            string drawString = "Sample Text";
+            System.Drawing.Font drawFont = new System.Drawing.Font("Arial", 16);
+            System.Drawing.SolidBrush drawBrush = new System.Drawing.SolidBrush(System.Drawing.Color.White);
+            float x = 150.0F;
+            float y = 50.0F;
+
+
+            SizeF size = g.MeasureString(drawString, drawFont);
+            g.FillRectangle(new System.Drawing.SolidBrush(System.Drawing.Color.Black), new Rectangle((int)x, (int)y, (int)size.Width, (int)size.Height));
+
+
+            System.Drawing.StringFormat drawFormat = new System.Drawing.StringFormat();
+            g.DrawString(drawString, drawFont, drawBrush, x, y, drawFormat);
+            drawFont.Dispose();
+            drawBrush.Dispose();
+
+            g.Dispose();
+            ReleaseDC(IntPtr.Zero, desktopPtr);
+        }
+    }
+}
