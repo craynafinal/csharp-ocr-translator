9c868adcb765d9eca6ef872a5f89e7b75adc5547
craynafinal <craynafinal@gmail.com>
2019-02-16 21:40:51 -0800

    designed the config editor

---
+++ DesktopApp/Forms/ConfigEditor.Designer.cs
@@ -0,0 +1,372 @@
+﻿namespace DesktopApp.Forms
+{
+    partial class ConfigEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.ScreenXTextBox = new System.Windows.Forms.TextBox();
+            this.ScreenYTextBox = new System.Windows.Forms.TextBox();
+            this.ScreenYLabel = new System.Windows.Forms.Label();
+            this.OutputYTextBox = new System.Windows.Forms.TextBox();
+            this.OutputYLabel = new System.Windows.Forms.Label();
+            this.OutputXTextBox = new System.Windows.Forms.TextBox();
+            this.OutputXLabel = new System.Windows.Forms.Label();
+            this.ScreenWTextBox = new System.Windows.Forms.TextBox();
+            this.ScreenWLabel = new System.Windows.Forms.Label();
+            this.ScreenHTextBox = new System.Windows.Forms.TextBox();
+            this.ScreenHLabel = new System.Windows.Forms.Label();
+            this.OutputHTextBox = new System.Windows.Forms.TextBox();
+            this.OutputHLabel = new System.Windows.Forms.Label();
+            this.OutputWTextBox = new System.Windows.Forms.TextBox();
+            this.OutputWLabel = new System.Windows.Forms.Label();
+            this.BGColorLabel = new System.Windows.Forms.Label();
+            this.FontColorLabel = new System.Windows.Forms.Label();
+            this.FontSizeTextBox = new System.Windows.Forms.TextBox();
+            this.FontSizeLabel = new System.Windows.Forms.Label();
+            this.FontLabel = new System.Windows.Forms.Label();
+            this.TargetLangLabel = new System.Windows.Forms.Label();
+            this.SourceLangLabel = new System.Windows.Forms.Label();
+            this.ScreenXLabel = new System.Windows.Forms.Label();
+            this.FontDropdown = new System.Windows.Forms.ComboBox();
+            this.FontColorDropdown = new System.Windows.Forms.ComboBox();
+            this.BGColorDropdown = new System.Windows.Forms.ComboBox();
+            this.TargetLangDropdown = new System.Windows.Forms.ComboBox();
+            this.SourceLangDropdown = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(645, 186);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 1;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // ScreenXTextBox
+            // 
+            this.ScreenXTextBox.Location = new System.Drawing.Point(83, 12);
+            this.ScreenXTextBox.Name = "ScreenXTextBox";
+            this.ScreenXTextBox.Size = new System.Drawing.Size(100, 21);
+            this.ScreenXTextBox.TabIndex = 2;
+            // 
+            // ScreenYTextBox
+            // 
+            this.ScreenYTextBox.Location = new System.Drawing.Point(83, 52);
+            this.ScreenYTextBox.Name = "ScreenYTextBox";
+            this.ScreenYTextBox.Size = new System.Drawing.Size(100, 21);
+            this.ScreenYTextBox.TabIndex = 4;
+            // 
+            // ScreenYLabel
+            // 
+            this.ScreenYLabel.Location = new System.Drawing.Point(11, 56);
+            this.ScreenYLabel.Name = "ScreenYLabel";
+            this.ScreenYLabel.Size = new System.Drawing.Size(66, 12);
+            this.ScreenYLabel.TabIndex = 3;
+            this.ScreenYLabel.Text = "Screen Y";
+            this.ScreenYLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // OutputYTextBox
+            // 
+            this.OutputYTextBox.Location = new System.Drawing.Point(262, 52);
+            this.OutputYTextBox.Name = "OutputYTextBox";
+            this.OutputYTextBox.Size = new System.Drawing.Size(100, 21);
+            this.OutputYTextBox.TabIndex = 8;
+            // 
+            // OutputYLabel
+            // 
+            this.OutputYLabel.Location = new System.Drawing.Point(190, 56);
+            this.OutputYLabel.Name = "OutputYLabel";
+            this.OutputYLabel.Size = new System.Drawing.Size(66, 12);
+            this.OutputYLabel.TabIndex = 7;
+            this.OutputYLabel.Text = "Output Y";
+            this.OutputYLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // OutputXTextBox
+            // 
+            this.OutputXTextBox.Location = new System.Drawing.Point(262, 12);
+            this.OutputXTextBox.Name = "OutputXTextBox";
+            this.OutputXTextBox.Size = new System.Drawing.Size(100, 21);
+            this.OutputXTextBox.TabIndex = 6;
+            // 
+            // OutputXLabel
+            // 
+            this.OutputXLabel.Location = new System.Drawing.Point(190, 16);
+            this.OutputXLabel.Name = "OutputXLabel";
+            this.OutputXLabel.Size = new System.Drawing.Size(66, 12);
+            this.OutputXLabel.TabIndex = 5;
+            this.OutputXLabel.Text = "Output X";
+            this.OutputXLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // ScreenWTextBox
+            // 
+            this.ScreenWTextBox.Location = new System.Drawing.Point(83, 92);
+            this.ScreenWTextBox.Name = "ScreenWTextBox";
+            this.ScreenWTextBox.Size = new System.Drawing.Size(100, 21);
+            this.ScreenWTextBox.TabIndex = 12;
+            // 
+            // ScreenWLabel
+            // 
+            this.ScreenWLabel.Location = new System.Drawing.Point(11, 96);
+            this.ScreenWLabel.Name = "ScreenWLabel";
+            this.ScreenWLabel.Size = new System.Drawing.Size(66, 12);
+            this.ScreenWLabel.TabIndex = 11;
+            this.ScreenWLabel.Text = "Screen W";
+            this.ScreenWLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // ScreenHTextBox
+            // 
+            this.ScreenHTextBox.Location = new System.Drawing.Point(83, 132);
+            this.ScreenHTextBox.Name = "ScreenHTextBox";
+            this.ScreenHTextBox.Size = new System.Drawing.Size(100, 21);
+            this.ScreenHTextBox.TabIndex = 16;
+            // 
+            // ScreenHLabel
+            // 
+            this.ScreenHLabel.Location = new System.Drawing.Point(11, 136);
+            this.ScreenHLabel.Name = "ScreenHLabel";
+            this.ScreenHLabel.Size = new System.Drawing.Size(66, 12);
+            this.ScreenHLabel.TabIndex = 15;
+            this.ScreenHLabel.Text = "Screen H";
+            this.ScreenHLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // OutputHTextBox
+            // 
+            this.OutputHTextBox.Location = new System.Drawing.Point(262, 132);
+            this.OutputHTextBox.Name = "OutputHTextBox";
+            this.OutputHTextBox.Size = new System.Drawing.Size(100, 21);
+            this.OutputHTextBox.TabIndex = 22;
+            // 
+            // OutputHLabel
+            // 
+            this.OutputHLabel.Location = new System.Drawing.Point(190, 136);
+            this.OutputHLabel.Name = "OutputHLabel";
+            this.OutputHLabel.Size = new System.Drawing.Size(66, 12);
+            this.OutputHLabel.TabIndex = 21;
+            this.OutputHLabel.Text = "Output H";
+            this.OutputHLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // OutputWTextBox
+            // 
+            this.OutputWTextBox.Location = new System.Drawing.Point(262, 92);
+            this.OutputWTextBox.Name = "OutputWTextBox";
+            this.OutputWTextBox.Size = new System.Drawing.Size(100, 21);
+            this.OutputWTextBox.TabIndex = 20;
+            // 
+            // OutputWLabel
+            // 
+            this.OutputWLabel.Location = new System.Drawing.Point(190, 96);
+            this.OutputWLabel.Name = "OutputWLabel";
+            this.OutputWLabel.Size = new System.Drawing.Size(66, 12);
+            this.OutputWLabel.TabIndex = 19;
+            this.OutputWLabel.Text = "Output W";
+            this.OutputWLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // BGColorLabel
+            // 
+            this.BGColorLabel.Location = new System.Drawing.Point(369, 136);
+            this.BGColorLabel.Name = "BGColorLabel";
+            this.BGColorLabel.Size = new System.Drawing.Size(72, 12);
+            this.BGColorLabel.TabIndex = 30;
+            this.BGColorLabel.Text = "BG Color";
+            this.BGColorLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // FontColorLabel
+            // 
+            this.FontColorLabel.Location = new System.Drawing.Point(369, 96);
+            this.FontColorLabel.Name = "FontColorLabel";
+            this.FontColorLabel.Size = new System.Drawing.Size(72, 12);
+            this.FontColorLabel.TabIndex = 28;
+            this.FontColorLabel.Text = "Font Color";
+            this.FontColorLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // FontSizeTextBox
+            // 
+            this.FontSizeTextBox.Location = new System.Drawing.Point(441, 52);
+            this.FontSizeTextBox.Name = "FontSizeTextBox";
+            this.FontSizeTextBox.Size = new System.Drawing.Size(100, 21);
+            this.FontSizeTextBox.TabIndex = 27;
+            // 
+            // FontSizeLabel
+            // 
+            this.FontSizeLabel.Location = new System.Drawing.Point(369, 56);
+            this.FontSizeLabel.Name = "FontSizeLabel";
+            this.FontSizeLabel.Size = new System.Drawing.Size(72, 12);
+            this.FontSizeLabel.TabIndex = 26;
+            this.FontSizeLabel.Text = "Font Size";
+            this.FontSizeLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // FontLabel
+            // 
+            this.FontLabel.Location = new System.Drawing.Point(369, 16);
+            this.FontLabel.Name = "FontLabel";
+            this.FontLabel.Size = new System.Drawing.Size(72, 12);
+            this.FontLabel.TabIndex = 24;
+            this.FontLabel.Text = "Font";
+            this.FontLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // TargetLangLabel
+            // 
+            this.TargetLangLabel.Location = new System.Drawing.Point(548, 56);
+            this.TargetLangLabel.Name = "TargetLangLabel";
+            this.TargetLangLabel.Size = new System.Drawing.Size(72, 12);
+            this.TargetLangLabel.TabIndex = 35;
+            this.TargetLangLabel.Text = "Out Lang";
+            this.TargetLangLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // SourceLangLabel
+            // 
+            this.SourceLangLabel.Location = new System.Drawing.Point(548, 16);
+            this.SourceLangLabel.Name = "SourceLangLabel";
+            this.SourceLangLabel.Size = new System.Drawing.Size(72, 12);
+            this.SourceLangLabel.TabIndex = 33;
+            this.SourceLangLabel.Text = "In Lang";
+            this.SourceLangLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // ScreenXLabel
+            // 
+            this.ScreenXLabel.Location = new System.Drawing.Point(11, 15);
+            this.ScreenXLabel.Name = "ScreenXLabel";
+            this.ScreenXLabel.Size = new System.Drawing.Size(66, 12);
+            this.ScreenXLabel.TabIndex = 37;
+            this.ScreenXLabel.Text = "Screen X";
+            this.ScreenXLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // FontDropdown
+            // 
+            this.FontDropdown.FormattingEnabled = true;
+            this.FontDropdown.Location = new System.Drawing.Point(441, 12);
+            this.FontDropdown.Name = "FontDropdown";
+            this.FontDropdown.Size = new System.Drawing.Size(100, 20);
+            this.FontDropdown.TabIndex = 39;
+            // 
+            // FontColorDropdown
+            // 
+            this.FontColorDropdown.FormattingEnabled = true;
+            this.FontColorDropdown.Location = new System.Drawing.Point(441, 93);
+            this.FontColorDropdown.Name = "FontColorDropdown";
+            this.FontColorDropdown.Size = new System.Drawing.Size(100, 20);
+            this.FontColorDropdown.TabIndex = 40;
+            // 
+            // BGColorDropdown
+            // 
+            this.BGColorDropdown.FormattingEnabled = true;
+            this.BGColorDropdown.Location = new System.Drawing.Point(441, 133);
+            this.BGColorDropdown.Name = "BGColorDropdown";
+            this.BGColorDropdown.Size = new System.Drawing.Size(100, 20);
+            this.BGColorDropdown.TabIndex = 41;
+            // 
+            // TargetLangDropdown
+            // 
+            this.TargetLangDropdown.FormattingEnabled = true;
+            this.TargetLangDropdown.Location = new System.Drawing.Point(620, 53);
+            this.TargetLangDropdown.Name = "TargetLangDropdown";
+            this.TargetLangDropdown.Size = new System.Drawing.Size(100, 20);
+            this.TargetLangDropdown.TabIndex = 43;
+            // 
+            // SourceLangDropdown
+            // 
+            this.SourceLangDropdown.FormattingEnabled = true;
+            this.SourceLangDropdown.Location = new System.Drawing.Point(620, 12);
+            this.SourceLangDropdown.Name = "SourceLangDropdown";
+            this.SourceLangDropdown.Size = new System.Drawing.Size(100, 20);
+            this.SourceLangDropdown.TabIndex = 42;
+            // 
+            // ConfigEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(741, 227);
+            this.Controls.Add(this.TargetLangDropdown);
+            this.Controls.Add(this.SourceLangDropdown);
+            this.Controls.Add(this.BGColorDropdown);
+            this.Controls.Add(this.FontColorDropdown);
+            this.Controls.Add(this.FontDropdown);
+            this.Controls.Add(this.ScreenXLabel);
+            this.Controls.Add(this.TargetLangLabel);
+            this.Controls.Add(this.SourceLangLabel);
+            this.Controls.Add(this.BGColorLabel);
+            this.Controls.Add(this.FontColorLabel);
+            this.Controls.Add(this.FontSizeTextBox);
+            this.Controls.Add(this.FontSizeLabel);
+            this.Controls.Add(this.FontLabel);
+            this.Controls.Add(this.OutputHTextBox);
+            this.Controls.Add(this.OutputHLabel);
+            this.Controls.Add(this.OutputWTextBox);
+            this.Controls.Add(this.OutputWLabel);
+            this.Controls.Add(this.ScreenHTextBox);
+            this.Controls.Add(this.ScreenHLabel);
+            this.Controls.Add(this.ScreenWTextBox);
+            this.Controls.Add(this.ScreenWLabel);
+            this.Controls.Add(this.OutputYTextBox);
+            this.Controls.Add(this.OutputYLabel);
+            this.Controls.Add(this.OutputXTextBox);
+            this.Controls.Add(this.OutputXLabel);
+            this.Controls.Add(this.ScreenYTextBox);
+            this.Controls.Add(this.ScreenYLabel);
+            this.Controls.Add(this.ScreenXTextBox);
+            this.Controls.Add(this.SaveButton);
+            this.Name = "ConfigEditor";
+            this.Text = "ConfigEditor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.TextBox ScreenXTextBox;
+        private System.Windows.Forms.TextBox ScreenYTextBox;
+        private System.Windows.Forms.Label ScreenYLabel;
+        private System.Windows.Forms.TextBox OutputYTextBox;
+        private System.Windows.Forms.Label OutputYLabel;
+        private System.Windows.Forms.TextBox OutputXTextBox;
+        private System.Windows.Forms.Label OutputXLabel;
+        private System.Windows.Forms.TextBox ScreenWTextBox;
+        private System.Windows.Forms.Label ScreenWLabel;
+        private System.Windows.Forms.TextBox ScreenHTextBox;
+        private System.Windows.Forms.Label ScreenHLabel;
+        private System.Windows.Forms.TextBox OutputHTextBox;
+        private System.Windows.Forms.Label OutputHLabel;
+        private System.Windows.Forms.TextBox OutputWTextBox;
+        private System.Windows.Forms.Label OutputWLabel;
+        private System.Windows.Forms.Label BGColorLabel;
+        private System.Windows.Forms.Label FontColorLabel;
+        private System.Windows.Forms.TextBox FontSizeTextBox;
+        private System.Windows.Forms.Label FontSizeLabel;
+        private System.Windows.Forms.Label FontLabel;
+        private System.Windows.Forms.Label TargetLangLabel;
+        private System.Windows.Forms.Label SourceLangLabel;
+        private System.Windows.Forms.Label ScreenXLabel;
+        private System.Windows.Forms.ComboBox FontDropdown;
+        private System.Windows.Forms.ComboBox FontColorDropdown;
+        private System.Windows.Forms.ComboBox BGColorDropdown;
+        private System.Windows.Forms.ComboBox TargetLangDropdown;
+        private System.Windows.Forms.ComboBox SourceLangDropdown;
+    }
+}
